d7706010b922a96d0c9878641d1d08d0a50a1fd0
Chadi Hammoud <79449894+Chadi-Hammoud@users.noreply.github.com>
2022-08-04 17:04:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
    function test(){
    fetch('https://fathomless-mesa-68337.herokuapp.com/data')
-  .then(response => response.json())
+  .then(response => response.json(
   .then(json => {
       var ul = document.getElementById('ul_01')
       json.forEach(element => {
@@ -9,6 +9,5 @@
           li.innerHTML = element.id;
           ul.appendChild(li);
 
-      });
-  })
+      });))
 }
